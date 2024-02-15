8fcebb705f0f8ba9c7a158fb62ac746a981bc989
Diogo Peixoto <diogosoarespeixoto41@gmail.com>
2024-02-15 08:12:05 -0300

    pagina de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./routes";
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import "./global.css";
+import { Toaster } from "sonner";
 
 export function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | pizza.shop" />
+      <Toaster position="top-right" richColors />
       <BrowserRouter>
         <Router />
       </BrowserRouter>
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,3 +1,55 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-label";
+import { useCallback } from "react";
+import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const signInForm = z.object({
+  email: z.string().email(),
+});
+
+type SignInForm = z.infer<typeof signInForm>;
+
 export const SignIn = () => {
-  return <h1>SignIn</h1>;
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>();
+
+  const handleSignIn = useCallback((data: SignInForm) => {}, []);
+
+  return (
+    <>
+      <Helmet title="Login" />
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar Painel
+            </h1>
+
+            <p className="text-sm text-muteds-foreground">
+              Acompanhe suas vendas pelo painel do parceiro!
+            </p>
+          </div>
+
+          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
+            <div className="space-y-2">
+              <Label className="font-semibold" htmlFor="email">
+                Seu e-mail
+              </Label>
+              <Input id="email" type="email" {...register("email")} />
+            </div>
+
+            <Button disabled={isSubmitting} className="w-full" type="submit">
+              Acessar Painel
+            </Button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
